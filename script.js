a68add56b78a80df26990997570b82fb2920366a
operator <152005902+0perat0-r@users.noreply.github.com>
2023-12-12 18:17:47 +0300

    Update to questions.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let q6 = prompt("What is 2 + 2?")
 let q7 = prompt("Neil Armstrong was the second man on the moon. Respond with True or False.")
 let q8 = prompt("What country is the largest?")
 let q9 = prompt("What is the approximate population of Earth? \n a: 7 billion \n b: 900 billion \n c: 10 million \n d: 6000")
-let q10 = prompt("Finish the word T - e - _")
+let q10 = prompt("What is the most consumed liquid in the world?")
 let q11 = prompt("Does HTML stand for Hyper Text Markup Language? Respond with True or False.")
 
 if (q1.toLowerCase() == "mercury") {points=points+1}
@@ -21,7 +21,7 @@ if (q6.toLowerCase() == '4') {points=points+1}
 if (q7.toLowerCase() == 'false') {points=points+1}
 if (q8.toLowerCase() == 'russia') {points=points+1}
 if (q9.toLowerCase() == 'a') {points=points+1}
-if (q10.toLowerCase() == 'a') {points=points+1}
+if (q10.toLowerCase() == 'water') {points=points+1}
 if (q11.toLowerCase() == 'true') {points=points+1}
 
 if (points >= 10) {alert(`You won! with ${points} points.`)} else {alert(`You got ${points} points. We thank you for trying.`)}};
